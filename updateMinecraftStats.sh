f3b13d7578b083e4c0600f730345b6bb8ebd0d53
m341772 <eric.martin@airfrance.fr>
2019-10-16 15:05:23 +0200

    feat: add updater script

---
+++ updateMinecraftStats.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# This script can be used to upgrade/compile to the latest version
+# Please not that all modifications on the original files are lost (but not your conf>. files)
+
+echo "Updating to server revision..."
+git fetch --all
+git reset --hard origin/master
+
+echo "Building"
+npm install
+
+sudo systemctl daemon-reload
+
+echo "Restarting backend"
+sudo service minecraftStats stop
+sleep 5
+sudo service destinyserver start
+
